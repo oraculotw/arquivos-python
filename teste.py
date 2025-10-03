cf369ba85e0fcb0ffc58abb281a94fb4d46a0b14
oraculotw <107263071+oraculotw@users.noreply.github.com>
2025-10-03 11:59:46 -0300

    ok

---
+++ teste.py
@@ -0,0 +1,18 @@
+print("Calaivos seres humanos")
+
+print(5+3)
+print(5-2)
+print(5*3)
+print(10/2)
+print(3**3)
+
+idade = 18
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+for i in range(5):
+    print("Número:", i)
+
+
